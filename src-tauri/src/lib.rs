2ff31a97c01177321f76f5d85fb33617f812a37f
Leonardo Geminiani <leonardo.geminiani7@gmail.com>
2024-12-17 14:27:01 +0100

    regata-comp

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,11 @@
+use tauri::{utils::config::WindowConfig, window, Manager, WebviewUrl};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { invoke } from '@tauri-apps/api/core';
+import { Window } from "@tauri-apps/api/window"
+import { Webview } from "@tauri-apps/api/webview"
+import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
+import { RegataComponent } from '../regata/regata.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RegataComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  clickFunc() {
+    const webview = new WebviewWindow('my-label', {
+      url: '#'
+    });
+    
+    webview.once('tauri://error', function (e) {
+      // an error happened creating the webview
+      console.error(e)
+     });
+    //invoke("create_window");
+  }
 }
