a74540b33a6269ded0e42be4b97a94dfb8bd455a
Leonardo Geminiani <leonardo.geminiani7@gmail.com>
2024-12-17 01:09:36 +0100

    bk

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { HeaderComponent } from "./components/header/header.component";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)],
